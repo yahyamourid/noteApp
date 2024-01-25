d28b2423c6b582cb75042e50efddd170e57d38a9
Yahya MOURID <128039351+yahyamourid@users.noreply.github.com>
2024-01-25 21:59:08 +0100

    Update Signup.js

--- src/components/noteApp/Signup.js
+++ src/components/noteApp/Signup.js
@@ -44,6 +44,7 @@ const Signup = () => {
         setShopwd(!showpwd)
     }
     return (
+        
     <div className='flex font-body bg-gradient-to-r from-[#fca311] items-center justify-center min-h-screen text-center'>
       <div className='flex   w-3/5  shadow-xl rounded-xl'>
         <div className='flex items-center flex-col bg-[#fca311] rounded-l-xl w-1/3'>
@@ -110,13 +111,13 @@ const Signup = () => {
 
       </div>
             
-             </div>
              <div className='absolute bottom-0 text-[#fca311] bg-[#14213d] text-lg w-full p-4 '>
+             
           <p>&copy; Created by Yahya-Mourid</p>
+            
+      </div >
       </div>
-    </div>
-    
-    
+      
 
   )
 }
