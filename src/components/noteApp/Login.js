8f91641ea6f5bbae2afef8b83b3094fb01409c68
Yahya MOURID <128039351+yahyamourid@users.noreply.github.com>
2024-01-26 11:22:24 +0100

    Update Login.js

--- src/components/noteApp/Login.js
+++ src/components/noteApp/Login.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { FiMail, FiKey, FiEye } from 'react-icons/fi'
-import FaSpinner from 'react-icons/fa'
+import {FaSpinner} from 'react-icons/fa'
 
 const Login = () => {
   const [data, setData] = useState({
